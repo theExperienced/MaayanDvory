9e105f1d3050dc5cabebd873b27e78ec5c171020
theExperienced <mizradan2@gmail.com>
2021-02-03 13:20:35 +0200

    gem photos added + gem grid

--- js/index.js
+++ js/index.js
@@ -19,3 +19,41 @@ VanillaTilt.init(document.querySelectorAll('.header__niche'), {
 
 // document.querySelector('.container').style.height = document.querySelector('.container').getBoundingClientRect.height + window.innerHeight
 
+const headings = ['gore', 'shmore', 'lore'];
+for (let i = 0; i < 15; i++) {
+    const div = document.createElement('div');
+    div.className = `gallery__gem gallery__gem--${i + 1}`;
+    
+    const container = document.createElement('div');
+    container.className = `gallery__gem-container gallery__gem-container--${i + 1}`;
+
+    const h3 = document.createElement('h3');
+    h3.className = `gallery__gem-heading gallery__gem-heading--${i + 1}`;
+    h3.innerHTML = headings[Math.floor(Math.random() * 3)];
+
+    const p = document.createElement('p');
+    p.innerHTML = 'Lorem ipsum dolor sit amet.'
+    p.className = `gallery__gem-desc gallery__gem-desc--${i + 1}`;
+
+    const p2 = document.createElement('p2');
+    p2.innerHTML = 'ש"ח ' + `${Math.floor(Math.random() * 350)}`;
+    p2.className = `gallery__gem-price gallery__gem-price--${i + 1}`;
+
+    const img = document.createElement('img');
+    if(i > 7) {
+        img.className = `gallery__gem-img gallery__gem-img--${i + 1}`;
+        img.setAttribute('src', `https://picsum.photos/id/${i * 2 + 1}/450/450`);
+    }
+    else {
+        img.className = `gallery__gem-img gallery__gem-img--${i + 1}`;
+        img.setAttribute('src', `./imgs/gems/gem${i + 1}.jpeg`);
+    }
+
+    container.appendChild(h3);
+    container.appendChild(p);
+    container.appendChild(p2);
+    div.appendChild(img);
+
+    div.appendChild(container);
+    document.querySelector('.gallery__grid').appendChild(div);
+}
